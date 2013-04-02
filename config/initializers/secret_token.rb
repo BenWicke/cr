4fe3547f7b3e636ec2cdc5830ceafeb8f8816cf9
Benjamin Wicke <ben@ciagent.com>
2013-04-02 11:35:09 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Costrecovery::Application.config.secret_token = '3096fcec041e05fabd876ae828c21c3a10c7855b203d390857e51ef914fc41b8cabaae27eed0cbcda81529cd202309adbaa5a645fe17079f21a4bdffe0b8d041'
