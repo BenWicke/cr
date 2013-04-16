f2d9ffb1f92992bbe8798354f6201e2e0776d188
Benjamin Wicke <ben@ciagent.com>
2013-04-16 16:11:10 -0400

    deleted costrecovery.dump from app directory

--- app/mailers/pass_reset.rb
+++ app/mailers/pass_reset.rb
@@ -1,5 +1,5 @@
 class PassReset < ActionMailer::Base
-  default :from => "finne@ciagent.com"
+  default :from => "ben@ciagent.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
