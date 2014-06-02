63bc14cbaa1bc943c62d3a89a7da0f144325b826
Benjamin Wicke <ben@ciagent.com>
2014-06-02 09:51:58 -0400

    syntax error

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,6 +4,6 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Costrecovery::Application.config.secret_token = ENV['APP_SECRET’]
+Costrecovery::Application.config.secret_token = ENV['APP_SECRET']
 
 
